a2325771241ad904083ad736bf298f394c9d8a4e
Tai @ Flex <dch.tai@gmail.com>
2016-12-05 04:18:37 +0000

    initial upwards teleport added

--- abm.lua
+++ abm.lua
@@ -1,8 +1,21 @@
-minetest.register_abm({ -- going up
-	func = function(pos)
+-- Going up
+
+minetest.register_abm({
+	nodenames = {"skytardis:tardiswood"},
+	interval = 1,
+	chance = 1,
+	action = function(pos)
 		-- check for player 1 node away
-		-- if player is owner, transport
-		-- else, check for free-for-all node
-		-- if active, transport
+		for obj in pairs( minetest.get_objects_inside_radius(pos,1) ) do
+			if obj:is_player() then
+				local playername = obj:get_player_name()
+				local bounds = skytardis:getSkyBoundsFor(pos.x,pos.z)
+				if skytardis:checkOwnerAt(bounds.pos1,playername) then
+					-- if player is owner, transport
+					obj:moveto(skytardis:getSkyStructureFor(pos.x,pos.z) )
+				end
+				-- TODO add system to have guests.... rely on nearby protector to provide this info?
+			end
+		end
 	end
 })
