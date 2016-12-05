3632df40df0fe45fcb8c75e2214c8f31e791132b
Tai @ Flex <dch.tai@gmail.com>
2016-12-05 03:39:33 +0000

    adjusted notes, grammer, moduels not ready

--- protection.lua
+++ protection.lua
@@ -1,11 +1,11 @@
 -- Register protection of the sky building
--- only the owner can have protection
+-- only the owner can have protection, allowing them to modify the sky tardis area
 -- maybe have a TARDIS Key item which allows another player to interact
 
 local oldprotect = minetest.is_protected
 
-function tardisprotect = function(pos,owner)
-	local tardisbounds = skytardis.getBoundsAt(pos)
+local tardisprotect = function(pos,owner)
+	local tardisbounds = skytardis:getBoundsAt(pos)
 
 	-- pass it on
 	return oldprotect(pos,owner)
