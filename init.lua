48e7942d515f2850568de84c2e6f140d9d59ff8b
Tai @ Flex <dch.tai@gmail.com>
2016-12-05 03:38:45 +0000

    load new files

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 skytardis = {}
 
 local path = minetest.get_modpath("skytardis")
-dofile(path.."/nodes.lua")
+--dofile(path.."/abm.lua")
+--dofile(path.."/protection.lua")
+dofile(path.."/skybuilding.lua")
 dofile(path.."/coordmapping.lua")
+dofile(path.."/nodes.lua")
